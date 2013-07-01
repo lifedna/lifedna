5b18e63672bce635737ea2e517765ec9aac118d6
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-07-01 21:04:20 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifedna::Application.config.secret_token = '0eb22cd77d79d683626df27755d1eacaedba823041e6e2caa5d136dcbff324ab54b23d2531f5b2f9977b3856e0a3490bb607cc250b554ad01fb3161f58cf9a0d'
